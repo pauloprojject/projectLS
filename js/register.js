95290493da770d5e9c841895daad77396106660b
Paulo Victor <59893766+pauloprojject@users.noreply.github.com>
2020-12-13 18:56:55 -0300

    a

--- js/register.js
+++ js/register.js
@@ -1,10 +1,6 @@
-import datauri from '/Users/PICHAU/node_modules/datauri'
-// let con = await datauri('img/jeff.jpeg')
-// console.log(con)
 const btn = document.querySelector('#btn')
 btn.addEventListener('click', async e =>{
     e.preventDefault()
     let a = document.querySelector('#img')
-    let con = await datauri(a.value)
     console.log(con)
 })
