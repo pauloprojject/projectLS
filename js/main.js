26fe70cb8865afc59ede68d66b5ab8c2eb8f3ef9
Alecsander Camilo <alecsandercamilo@gmail.com>
2020-12-13 20:15:57 -0300

    Reverting

--- js/main.js
+++ js/main.js
@@ -1,41 +1,18 @@
 const coinsContainer = document.querySelector('#listaprod')
 // const prodi = prod
 prod.map(coin => {
-  // console.log(coin.Name)
+  console.log(coin.Name)
 })
 
 const coinsHTML = prod.map(products => {
     return `<div class="prod">
           <a href="pages/${products.Name}.html">
-          <img src="${products.ImgUrl}">
+          <img src="${products.ImgUrl}"></a>
           <p class="nome-prod">${products.Name}</p>
           <p class="quant">Quantidade: ${products.Quantidade}</p>
-          </a>
+          
         </div>`
   })
   .join('')
 
-// coinsContainer.innerHTML = coinsHTML
-
-
-let inserir = () => {
-  let a = document.querySelector('.descricao span')
-  let b = document.querySelector('.descricao h1')
-  let c = document.querySelector('.descricao p')
-  let d = document.querySelector('.preco span')
-  let e = document.querySelector('.detalhes p')
-  let url = window.location.href
-  f = url.split('/')
-  g = f[4].split('.')
-  prod.map(item => {
-    if (item.Name == g[0]){
-      a.innerHTML = item.IdProduto
-      b.innerHTML = item.Name
-      c.innerHTML = item.FullName
-      d.innerHTML = item.Value
-      e.innerHTML = item.Description
-    }
-  })
-}
-
-inserir()
+coinsContainer.innerHTML = coinsHTML
--- js/register.js
+++
@@ -1,6 +0,0 @@
-const btn = document.querySelector('#btn')
-btn.addEventListener('click', async e =>{
-    e.preventDefault()
-    let a = document.querySelector('#img')
-    console.log(con)
-})
