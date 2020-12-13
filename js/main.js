731c5e586c95c39f8817b6e274ba07e7a0b1107d
Paulo Victor <59893766+pauloprojject@users.noreply.github.com>
2020-12-13 19:27:42 -0300

    a

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const coinsContainer = document.querySelector('#listaprod')
 // const prodi = prod
 prod.map(coin => {
-  console.log(coin.Name)
+  // console.log(coin.Name)
 })
 
 const coinsHTML = prod.map(products => {
@@ -15,4 +15,27 @@ const coinsHTML = prod.map(products => {
   })
   .join('')
 
-coinsContainer.innerHTML = coinsHTML
+// coinsContainer.innerHTML = coinsHTML
+
+
+let inserir = () => {
+  let a = document.querySelector('.descricao span')
+  let b = document.querySelector('.descricao h1')
+  let c = document.querySelector('.descricao p')
+  let d = document.querySelector('.preco span')
+  let e = document.querySelector('.detalhes p')
+  let url = window.location.href
+  f = url.split('/')
+  g = f[4].split('.')
+  prod.map(item => {
+    if (item.Name == g[0]){
+      a.innerHTML = item.IdProduto
+      b.innerHTML = item.Name
+      c.innerHTML = item.FullName
+      d.innerHTML = item.Value
+      e.innerHTML = item.Description
+    }
+  })
+}
+
+inserir()
